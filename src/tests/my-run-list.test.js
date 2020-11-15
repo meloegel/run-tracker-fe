35611788667406e55e092609ac96336709eadc02
meloegel <meloegel@gmail.com>
2020-11-15 16:16:45 -0500

    added test files

---
+++ src/tests/my-run-list.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+
+
+describe('test', () => {
+    test('test', () => {
+
+    })
+})
---
+++ src/tests/profile.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import Login from '../components/profile/Login';
+import Register from '../components/profile/Registration'
+
+describe('Tests', () => {
+    test('User can create account', () => {
+
+    })
+})
