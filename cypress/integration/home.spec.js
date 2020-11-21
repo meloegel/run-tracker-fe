ccc4c25d624023e6ac8127c0dea4607d3aacbed0
meloegel <meloegel@gmail.com>
2020-11-21 13:21:16 -0500

    working on home page tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,1 +1,9 @@
 // Home Page Tests //
+
+describe('Home page tests', () => {
+    it('can navigate to Home page', () => {
+        cy.visit('http://localhost:3000/')
+        cy.url().should('include', "localhost")
+    })
+
+})
