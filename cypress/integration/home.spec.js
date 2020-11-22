72cef01197f335d67e3613e18a8fd7c8073b4da0
meloegel <meloegel@gmail.com>
2020-11-21 19:45:01 -0500

    added home tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -6,6 +6,15 @@ describe('Home page tests', () => {
         cy.url().should('include', "localhost")
     })
     it('published runs should be displayed', () => {
-
+        cy.contains('Run Time').should('exist')
+    })
+    it('Login button routes correctly', () => {
+        cy.get('button.login').click()
+        cy.url().should('include', 'http://localhost:3000/login')
+    })
+    it('Register button routes correctly', () => {
+        cy.visit('http://localhost:3000/')
+        cy.get('button.register').click()
+        cy.url().should('include', 'http://localhost:3000/register')
     })
 })
